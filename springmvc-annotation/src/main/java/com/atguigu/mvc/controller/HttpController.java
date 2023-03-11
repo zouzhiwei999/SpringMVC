0607b0507c9f4b06d3c86c69a94713ccc3a878f2
zzw <1830560034@qq.com>
2023-03-12 00:01:57 +0800

    hot-fix first commit

--- springmvc-annotation/src/main/java/com/atguigu/mvc/controller/HttpController.java
+++ springmvc-annotation/src/main/java/com/atguigu/mvc/controller/HttpController.java
@@ -23,6 +23,7 @@ public class HttpController {
     public String test() {
         System.out.println(1/0);
         System.out.println("test");
+        System.out.println("hot-fix");
         return "success";
     }
 
